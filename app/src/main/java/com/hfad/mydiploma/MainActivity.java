a4f2447340bbbfd81e430480ee4c1d734835961e
ananasann <paramonovaann39@gmail.com>
2021-04-23 01:49:44 +0300

    Integrated google to sign in using firebase, the acc data is displayed in the layout

--- app/src/main/java/com/hfad/mydiploma/MainActivity.java
+++ app/src/main/java/com/hfad/mydiploma/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hfad.mydiploma;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,14 +8,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.AccountPicker;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.sql.Driver;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,16 +43,38 @@ public class MainActivity extends AppCompatActivity {
 
     EditText ETemail;
     EditText ETpassword;
+    private GoogleSignInClient mSignInClient;
+    private static final int RC_SIGN_IN = 9001;
+    SignInButton signInButton;
+    @Nullable public GoogleSignInAccount currAcc;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.authorization);
+        setContentView(R.layout.auth_google);
         getSupportActionBar().hide();
 
         ETemail = (EditText) findViewById(R.id.email);
         ETpassword = (EditText) findViewById(R.id.password);
+
+        GoogleSignInOptions gso =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .setHostedDomain("miem.hse.ru")
+                        .build();
+
+        mSignInClient = GoogleSignIn.getClient(this, gso);
+        signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = mSignInClient.getSignInIntent();
+                startActivityForResult(intent, RC_SIGN_IN);
+                Log.d("blya","");
+            }
+        });
+
 
         /*BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -48,6 +85,39 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);*/  //БЫЛО ТУТ ИЗНАЧАЛЬНО, НО ТАК РАБОТАТЬ НЕ БУДЕТ, ПОТОМУ ЧТО МЫ СНАЧАЛА ВЫЗЫВАЕМ МАКЕТ АВТОРИЗАЦИИ
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task =
+                    GoogleSignIn.getSignedInAccountFromIntent(data);
+            Log.d("tag","");
+            if (task.isSuccessful()) {
+                // Sign in succeeded, proceed with account
+                currAcc = task.getResult();
+
+                setContentView(R.layout.activity_main);
+                getSupportActionBar().show();
+
+                //ТО, ЧТО НИЖЕ И КАСАЕТСЯ БОТНАВ - ТУТ ТОЖЕ БЫТЬ НЕ ДОЛЖНО, ПОТОМУ ЧТО ВЫЗЫВАЕТСЯ ПРИ КЛИКЕ, А НАМ НЕ НУЖНО ЗАХОДИТЬ КАЖДЫЙ РАЗ
+                BottomNavigationView navView = findViewById(R.id.nav_view);
+                // Passing each menu ID as a set of Ids because each
+                // menu should be considered as top level destinations.
+                AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                        R.id.navigation_theory, R.id.navigation_tests, R.id.navigation_account)
+                        .build();
+                NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+                NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+                NavigationUI.setupWithNavController(navView, navController);
+            } else {
+                // Sign in failed, handle failure and update UI
+                // ...
+                Toast.makeText(this, "Неудалось войти", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 
@@ -76,5 +146,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Неправильно введен логин или пароль", Toast.LENGTH_SHORT).show();
         }
     }
+
 
 }
--- app/src/main/java/com/hfad/mydiploma/ui/account/AccountFragment.java
+++ app/src/main/java/com/hfad/mydiploma/ui/account/AccountFragment.java
@@ -1,9 +1,12 @@
 package com.hfad.mydiploma.ui.account;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,24 +15,78 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.hfad.mydiploma.MainActivity;
 import com.hfad.mydiploma.R;
+
+import java.util.concurrent.Executor;
 
 public class AccountFragment extends Fragment {
 
-    private AccountViewModel accountViewModel;
+    TextView logOut;
+    TextView fmo;
+    TextView group;
+    ImageView accImage;
+    private GoogleSignInClient mSignInClient;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        accountViewModel =
-                new ViewModelProvider(this).get(AccountViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_account, container, false);
-        final TextView textView = root.findViewById(R.id.FMO);
-        accountViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        fmo = root.findViewById(R.id.FMO);
+        group = root.findViewById(R.id.group);
+        accImage = root.findViewById(R.id.accImage);
+        logOut = root.findViewById(R.id.logOut);
+        return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        GoogleSignInAccount buffAcc = ((MainActivity) getActivity()).currAcc;
+        bind(buffAcc);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mSignInClient = GoogleSignIn.getClient(getContext(), gso);
+        logOut.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                mSignInClient.signOut();
             }
         });
-        return root;
+    }
+
+    private void bind(@Nullable GoogleSignInAccount acc) {
+        if (acc == null) {
+            Log.d("check", "null");
+        } else {
+            fmo.setText(acc.getDisplayName());
+            group.setText(acc.getEmail());
+
+            RequestOptions requestOptions = new RequestOptions();
+            requestOptions = requestOptions.transform(new RoundedCorners(180));
+            Glide
+                    .with(this)
+                    .load(acc.getPhotoUrl())
+                    .apply(requestOptions)
+                    .error(R.drawable.ic_account_black_24dp)
+                    .into(accImage);
+
+
+            Log.d("check", "nenull");
+        }
     }
 }
